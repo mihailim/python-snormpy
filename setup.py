15d7d08ce7784b8e3dfe82553d4e7214534460c0
Mihai Limbășan <mihai@limbasan.ro>
2014-03-14 16:19:03 +0200

    Updated setup script, moved source to src subdirectory.

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
- 
+
 setup(name='snormpy',
-    version="0.5.3",
-    description='Wrapper around pysnmp4 for easier snmp querying',
+    version='0.5.4',
+    description='Wrapper around pysnmp4 for easier SNMP querying',
     author='Dennis Kaarsemaker, Mike Bryant, Mihai Limbășan',
     author_email='mihailim@users.noreply.github.com',
-    py_modules=['snormpy'],
     url='https://github.com/mihailim/python-snormpy',
+    download_url='https://github.com/mihailim/python-snormpy/releases',
+    license='GPL3',
+    keywords='snmp',
     classifiers=[
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "Operating System :: OS Independent",
-        "Topic :: System :: Monitoring",
-        "Topic :: Software Development"
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: System :: Monitoring',
+        'Topic :: Software Development'
     ],
-    #install_requires='pysnmp',
+    package_dir={'': 'src'},
+    py_modules=['snormpy'],
+    install_requires=['pysnmp>=4.2.3', 'pysnmp-mibs'],
 )
